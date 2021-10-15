0818380dc515988b0458818e4c5e6637385eec1a
Maksym Kupriichuk <maksym.kupriichuk@gmail.com>
2021-10-15 18:35:28 +0300

    update types

--- index.d.ts
+++ index.d.ts
@@ -2,13 +2,13 @@
  * Return a function or functionWithThrottle
  * @param {Function} func function which you want to wrap
  * @param {number} ms number of delay
- * @param {{leading: boolean, trailing: boolean}|undefined} [options] options
+ * @param {{leading: boolean, trailing: boolean}|undefined|null} [options] options
  * @param {boolean|{merge: boolean, exp: boolean}} [condition] condition
  * @returns {Function} Return a function or functionWithThrottle
  */
 export function throttleOnWeak(
 	func: Function,
-	ms: number, options?: {leading?: boolean, trailing?: boolean},
+	ms: number, options?: {leading?: boolean, trailing?: boolean}|undefined|null,
 	condition?: boolean | {merge: boolean, exp: boolean})
 	: Function
 
@@ -16,10 +16,10 @@ export function throttleOnWeak(
  * Return a function or functionWithDebounce
  * @param {Function} func function which you want to wrap
  * @param {number} ms number of delay
- * @param {{leading: boolean, trailing: boolean, maxWait: number}|undefined} [options] options
+ * @param {{leading: boolean, trailing: boolean, maxWait: number}|undefined|null} [options] options
  * @param {boolean|{merge: boolean, exp: boolean}} [condition] condition
  * @returns {Function} Return a function or functionWithDebounce
  */
 export function debounceOnWeak(func: Function,
-ms: number, options?: {leading?: boolean, trailing?: boolean, maxWait?: number}|undefined,
+ms: number, options?: {leading?: boolean, trailing?: boolean, maxWait?: number}|undefined|null,
 condition?: boolean | {merge: boolean, exp: boolean}): Function
